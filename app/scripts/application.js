8e43a7a53e5ac55e4bff738c3093544ccb38d317
Grant Stampfli <mail@gstampfli.com>
2014-07-08 20:15:06 -0700

    adding project files to get started w/ logins and testing, still need to set up travis and other testinging srv. got back end framework init based on bookshelf and admit one

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,3 +1,56 @@
 'use strict';
 
-window.EmberSharing = Ember.Application.create();
+var App = window.App = Ember.Application.create();
+App.AdmitOneContainers = {}; //overridable by tests
+Ember.AdmitOne.setup( { containers: App.AdmitOneContainers });
+require('./models.js')(App);
+require('./router.js')(App);
+
+App.ApplicationAdapter = DS.RESTAdapter.extend({
+  namespace: 'api'
+});
+
+App.LoginController = Ember.Controller.extend({
+  actions: {
+    authenticate: function() {
+      var self = this;
+      var session = this.get('session');
+      var credentials = this.getProperties('username', 'password');
+      this.set('error', undefined);
+      this.set('password', undefined);
+      session.authenticate(credentials).then(function() {
+        var attemptedTransition = self.get('attemptedTransition');
+        if (attemptedTransition) {
+          attemptedTransition.retry();
+          self.set('attemptedTransition', null);
+        } else {
+          self.transitionToRoute('profile');
+        }
+      })
+      .catch(function(error) {
+        self.set('error', error);
+      });
+    }
+  }
+});
+
+App.SignupController = Ember.ObjectController.extend({
+  actions: {
+    signup: function() {
+      var session = this.get('session');
+      var self = this;
+
+      this.set('error', undefined);
+      this.get('model').save() // create the user
+      .then(function() {
+        session.login({ username: self.get('model.username') });
+        self.transitionToRoute('profile');
+      })
+      .catch(function(error) {
+        if (error.responseJSON) { error = error.responseJSON; }
+        if (error.error) { error = error.error; }
+        self.set('error', error);
+      });
+    }
+  }
+});
